50c3aee98884694d001f6a62a79dd30e47ef7ae8
Juan Diego Rodríguez Guarín <60738511+juandifs00@users.noreply.github.com>
2021-05-08 22:11:15 -0500

    realizacion de distintas pruebas

--- Pruebas.js
+++ Pruebas.js
@@ -85,6 +85,53 @@ function factorialRecursivo (n) {
 	}
 	return n * factorialRecursivo (n-1); 
 }
-console.time("Factorial Recursivo");
-console.log(factorialRecursivo(10));
-console.timeEnd("Factorial Recursivo");
+//console.time("Factorial Recursivo");
+//console.log(factorialRecursivo(10));
+//console.timeEnd("Factorial Recursivo");
+
+const solucionRecursiva = (n) => {
+    if (n==1) {
+        return 1;
+    } else {
+        return solucionRecursiva(n - 1) + 5    
+    } 
+}
+
+const solucionEcuacuion = (n) => {
+    return (5 * n) - 4;
+}
+
+/*console.time("SolucionRecursiva");
+let resultado = solucionRecursiva(100);
+console.log(resultado);
+console.timeEnd("SolucionRecursiva");
+
+console.time("SolucionEcuacion");
+let solucion = solucionEcuacuion(100);
+console.log(solucion);
+console.timeEnd("SolucionEcuacion");
+*/
+
+/**
+ * ordenar de mayor a menor y luego de menor a mayor.
+ * @returns 
+ */
+/*
+const crearVector = () => {
+    let vector = [];
+    let tam = 10000;
+    for (let index = 0; index < tam; index++) {
+      let random = Math.random() * 1000;
+      vector.push(Math.round(random));
+    }
+    return vector;
+  };
+  
+  let vector_numeros = crearVector();
+  
+  console.log(vector_numeros);
+  */ 
+ /*
+console.log("buenas a todos");
+console.log(vector_numeros);
+*/
